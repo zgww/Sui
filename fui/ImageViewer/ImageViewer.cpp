b7cd2d26ed5cbd9f94d46e5a8be7a5244b8ca281
lishuijun <645789309@qq.com>
2026-09-03 15:13:36 +0800

    ImageViewer 右键打开图片

--- fui/ImageViewer/ImageViewer.cpp
+++ fui/ImageViewer/ImageViewer.cpp
@@ -1,6 +1,7 @@
 #include <Windows.h>
 #include <string>
 #include <stdio.h>
+#include "Naga/Utf8Util.h"
 
 import ImageViewerApp;
 
@@ -8,24 +9,30 @@ void AppMain() {
 
 }
 
-int WinMain(
+int wWinMain(
 	HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
-	LPSTR     lpCmdLine,
+	PWSTR      lpCmdLine,
 	int       nShowCmd
 ) {    
-	//FreeConsole();//解除ide/vscode的控制台。否则AllocConsole无效
-	//AllocConsole();
-	//freopen("CONOUT$", "w", stdout);
-	//printf("lpCmdLine:%s\n", lpCmdLine);
-
-	std::string initialFile = lpCmdLine;
+	FreeConsole();//解除ide/vscode的控制台。否则AllocConsole无效
+	AllocConsole();
+	freopen("CONOUT$", "w", stdout);
+	int argc = 0;
+	auto newArgs = CommandLineToArgvW(lpCmdLine, &argc);
+	std::string initialFile;
+	//initialFile = "F:\\ws\\Sui\\fui\\fui\\x64\\Release\\asset\\sample.png";
+	if (argc > 0) {
+		initialFile = Utf8Util::toutf8(newArgs[0]);
+	}
+	LocalFree(newArgs);
+	printf("WinMainW lpCmdLine2:%s. nShowCmd:%d; argc:%d \n", initialFile.c_str(), nShowCmd, argc);
 	runImageViewer(initialFile);
 	return 0;
 }
 
 int main(int argc, char* argv[]) {
-	std::string initialFile;
+	std::string initialFile = "F:\\ws\\Sui\\fui\\fui\\x64\\Release\\asset\\sample.png";
 	if (argc > 1) {
 		initialFile = argv[1];
 	}
